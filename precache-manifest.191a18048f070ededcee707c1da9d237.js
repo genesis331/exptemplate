c202333b7ded69d7614bcc40557c0ab490b1152b
Cheah Zixu <cheah3838@gmail.com>
2020-09-12 03:04:17 +0000

    Updates

---
+++ precache-manifest.191a18048f070ededcee707c1da9d237.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eda879f3e68b036cca85c18ff1bc829",
+    "url": "/exptemplate/index.html"
+  },
+  {
+    "revision": "2863ee6d549c1e0a844f",
+    "url": "/exptemplate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb96d6c007ce29f443e7",
+    "url": "/exptemplate/static/js/2.6363989c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exptemplate/static/js/2.6363989c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2863ee6d549c1e0a844f",
+    "url": "/exptemplate/static/js/main.aa9cc01d.chunk.js"
+  },
+  {
+    "revision": "15e4f3bcf5d7251acda4",
+    "url": "/exptemplate/static/js/runtime-main.0256fce0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/exptemplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
